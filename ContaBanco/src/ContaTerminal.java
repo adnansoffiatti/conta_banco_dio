ded9ad843c2982eb5c361163df6c66b84f5f967a
adnansoffiatti <79457983+adnansoffiatti@users.noreply.github.com>
2023-05-15 12:07:13 -0300

    resolução do desafio de projeto - Conta Bancária

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,29 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner sc = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Bem-vindo!\nDigite o número da Agência:");
+        String agencia = sc.next();
+
+        System.out.println("Digite o número da Conta:");
+        int conta = sc.nextInt();
+
+        System.out.println("Digite o seu nome:");
+        String nome = sc.next();
+
+        System.out.println("Digite o saldo:");
+        double saldo = sc.nextDouble();
+
+        System.out.println
+        ("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " 
+            + agencia + ", conta " 
+            + conta + " e seu saldo " 
+            + saldo + " já está disponível para saque.");
+
+        sc.close();
     }
 }
